48e29b2f1916ea48c39151f5770e6e0603a10358
Pascal Ntaganda <pntaga@uw.edu>
2022-01-28 17:03:18 -0800

    import data

--- code/01_load_data.R
+++ code/01_load_data.R
@@ -1,6 +1,14 @@
 # Load data
-
 library(tidyverse)
 library(haven)
 library(here)
+
+## import data
+sec4  <- read_dta(here("raw_data", "Original_Data", "round1","SEC4.dta")) 
+sec5  <- read_dta(here("raw_data", "Original_Data", "round1","SEC5.dta")) 
+sec5a  <- read_dta(here("raw_data", "Original_Data", "round1","SEC5A.dta")) 
+sec6  <- read_dta(here("raw_data", "Original_Data", "round1","SEC6.dta")) 
+sec7  <- read_dta(here("raw_data", "Original_Data", "round1","SEC7.dta")) 
+sec9  <- read_dta(here("raw_data", "Original_Data", "round1","SEC9.dta")) 
+
 
