21db59ad870e469393d5f4367e69ede539c46553
Pascal Ntaganda <pntaga@uw.edu>
2022-02-03 10:22:01 -0800

    Add round 2

--- code/01_load_data.R
+++ code/01_load_data.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(haven)
 library(here)
 
+## round 1 ---- 
+
 ## roster data (interview info and results?) 
 round_1_interview_info <- read_dta(here(" raw_data", "round1", "Interview_info.dta "))
 round_1_interview_result <- read_dta(here(" raw_data", "round1", "interview_result.dta "))
@@ -18,5 +20,17 @@ round_1_sec6  <- read_dta(here( "raw_data", "round1", "SEC6.dta" ))
 round_1_sec7  <- read_dta(here( "raw_data", "round1", "SEC7.dta" )) 
 round_1_sec9  <- read_dta(here( "raw_data", "round1", "SEC9.dta" )) 
 
+## round 2 ---- 
+round_2_interview_info <- read_dta(here(" raw_data", "round2", "Interview_info.dta "))
+round_2_interview_result <- read_dta(here(" raw_data", "round2", "interview_result.dta "))
+round_2_cover  <- read_dta(here( "raw_data", "round2", "Cover.dta" )) 
 
+
+round_2_sec1 <- read_dta(here( "raw_data", "round2", "SEC1.dta" )) 
+round_2_sec4  <- read_dta(here( "raw_data", "round2", "SEC4.dta" )) 
+round_2_sec5  <- read_dta(here( "raw_data", "round2", "SEC5.dta" )) 
+round_2_sec5a  <- read_dta(here( "raw_data", "round2", "SEC5A.dta" )) 
+round_2_sec6  <- read_dta(here( "raw_data", "round2", "SEC6.dta" )) 
+round_2_sec7  <- read_dta(here( "raw_data", "round2", "SEC7.dta" )) 
+round_2_sec9  <- read_dta(here( "raw_data", "round2", "SEC9.dta" )) 
 
