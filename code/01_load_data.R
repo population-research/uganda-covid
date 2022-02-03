b22e6c3843879a4370ec81d008bb5b64d8ca15e6
Pascal Ntaganda <pntaga@uw.edu>
2022-02-03 10:11:32 -0800

    Update dataframe names and directory structure

--- code/01_load_data.R
+++ code/01_load_data.R
@@ -3,16 +3,20 @@ library(tidyverse)
 library(haven)
 library(here)
 
-## import raw data
-sec4  <- read_dta(here("raw_data", "original_Data", "round1","SEC4.dta")) 
-sec5  <- read_dta(here("raw_data", "original_Data", "round1","SEC5.dta")) 
-sec5a  <- read_dta(here("raw_data", "original_Data", "round1","SEC5A.dta")) 
-sec6  <- read_dta(here("raw_data", "original_Data", "round1","SEC6.dta")) 
-sec7  <- read_dta(here("raw_data", "original_Data", "round1","SEC7.dta")) 
-sec9  <- read_dta(here("raw_data", "original_Data", "round1","SEC9.dta")) 
-
 ## roster data (interview info and results?) 
-interview_info <- read_dta(here("raw_data", "original_Data", "round1","Interview_info.dta"))
-interview_result <- read_dta(here("raw_data", "original_Data", "round1","interview_result.dta"))
-cover  <- read_dta(here("raw_data", "original_Data", "round1","Cover.dta")) 
+round_1_interview_info <- read_dta(here(" raw_data", "round1", "Interview_info.dta "))
+round_1_interview_result <- read_dta(here(" raw_data", "round1", "interview_result.dta "))
+round_1_cover  <- read_dta(here( "raw_data", "round1", "Cover.dta" )) 
+
+
+## import raw data
+round_1_sec1 <- read_dta(here( "raw_data", "round1", "SEC1.dta" )) 
+round_1_sec4  <- read_dta(here( "raw_data", "round1", "SEC4.dta" )) 
+round_1_sec5  <- read_dta(here( "raw_data", "round1", "SEC5.dta" )) 
+round_1_sec5a  <- read_dta(here( "raw_data", "round1", "SEC5A.dta" )) 
+round_1_sec6  <- read_dta(here( "raw_data", "round1", "SEC6.dta" )) 
+round_1_sec7  <- read_dta(here( "raw_data", "round1", "SEC7.dta" )) 
+round_1_sec9  <- read_dta(here( "raw_data", "round1", "SEC9.dta" )) 
+
+
 
