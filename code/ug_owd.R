79f94aad786c587710cff10836a81d0dafc8d09a
PascalKryptonNtaganda <95452085+PascalKryptonNtaganda@users.noreply.github.com>
2022-08-05 10:59:50 -0700

    renamed variables in new cases dataset

--- code/ug_owd.R
+++ code/ug_owd.R
@@ -22,8 +22,8 @@ mutate(year = year(date), month = month(date), day = day(date)) # splitting date
 ## getting cases data by month and cases per 100,000 per month
 new_cases_monthly <- ug_data %>%
   group_by(year,month) %>% 
-    summarize(new_mon_cases = sum(new_cases), mean(population), mean(stringency_index), mean(reproduction_rate)) %>%
+    summarize(new_mon_cases = sum(new_cases), mean_population = mean(population), mean_stringency_index = (stringency_index), mean_reproduction_rate =  mean(reproduction_rate)) %>%
        mutate(
-           monthly_per_100000 = (new_mon_cases/ug_data$population[1]) * 100000
+           monthly_new_cases_per_100000 = (new_mon_cases/ug_data$population[1]) * 100000
   )
 
