f079be38c7dede1920fbe178cd3bb804166ec463
Claus Portner <cportner@seattleu.edu>
2022-08-09 08:40:23 -0700

    Add Oxford government restriction data
    Also shorten Covid cases code

---
+++ code/03_load_covid_cases_restrictions.R
@@ -0,0 +1,175 @@
+# Load data
+library(tidyverse)
+library(haven)
+library(here)
+library(janitor) # For data checking
+library(vtable) # For data checking
+library(labelled) # For data checking
+library(lubridate)
+
+# Functions
+rename_to_lower_snake <- function(df) {
+  df %>%
+    rename_with(~ gsub("([a-z])([A-Z])", "\\1_\\2", .x)) %>% # Adds _ to camel case var names
+    rename_with(~ tolower(gsub("[ /]+", "_", .x))) # Converts to lower and substitutes _ for spaces and /
+}
+
+Mode <- function(x, na.rm = FALSE) {
+  if (na.rm) {
+    x <- x[!is.na(x)]
+  }
+
+  ux <- unique(x)
+  return(ux[which.max(tabulate(match(x, ux)))])
+}
+
+# Covid data ----
+
+## Data source: OUR WORLDIN DATA
+## Description Link: https://docs.owid.io/projects/covid/en/latest/dataset.html
+## Data URL: https://covid.ourworldindata.org/data/owid-covid-data.csv
+
+## load data
+covid_cases <- read_csv(url("https://covid.ourworldindata.org/data/owid-covid-data.csv")) %>%
+  rename_to_lower_snake() %>%
+  filter(iso_code == "UGA") %>%
+  mutate( # splitting date into year month and day
+    year  = year(date),
+    month = month(date),
+    day   = day(date)
+  ) %>%
+  select(-date) %>%
+  # getting new cases by month and new cases per 100,000 per month
+  group_by(year, month) %>%
+  summarize(
+    cases_month       = sum(new_cases, na.rm = TRUE),
+    population        = mean(population, na.rm = TRUE),
+    stringency_index  = mean(stringency_index, na.rm = TRUE),
+    reproduction_rate = mean(reproduction_rate)
+  ) %>%
+  mutate(
+    cases_month_per_100000 = (cases_month / population) * 100000
+  ) %>%
+  select(-population)
+
+# Oxford Covid restriction measures ----
+
+# Documentation on coding
+# https://github.com/OxCGRT/covid-policy-tracker/blob/master/documentation/codebook.md#containment-and-closure-policies
+
+# Variable types:
+# C - containment and closure policies
+# E - economic policies
+# H - health system policies
+# V - vaccination policies
+# M - miscellaneous policies
+
+# We focus on C and E here because that is what is most likely to impact food
+# security
+
+# C1M_School closing	Record closings of schools and universities
+# C2M_Workplace closing	Record closings of workplaces
+# C3M_Cancel public events	Record cancelling public events
+# C4M_Restrictions on gatherings	Record limits on gatherings
+# C5M_Close public transport	Record closing of public transport
+# C6M_Stay at home requirements	Record orders to "shelter-in-place" and otherwise confine to the home
+# C7M_Restrictions on internal movement	Record restrictions on internal movement between cities/regions
+# C8 is for international travel; we ignore that
+
+# Flags for C:
+# Binary flag for geographic scope
+# 0 - targeted
+# 1- general
+# Blank - no data
+
+# E1_Income support (for households) Record if the government is providing
+# direct cash payments to people who lose their jobs or cannot work. Note: only
+# includes payments to firms if explicitly linked to payroll/salaries
+
+# E2_Debt/contract relief (for households) Record if the government is freezing
+# financial obligations for households (eg stopping loan repayments, preventing
+# services like water from stopping, or banning evictions)
+
+# E3_Fiscal measures Announced economic stimulus spending Note: only record
+# amount additional to previously announced spending; almost all 0 so drop
+
+# E4 is support to other countries; we ignore that
+
+# Flag for E1:
+# Binary flag for sectoral scope
+# 0 - formal sector workers only or informal sector workers only
+# 1 - all workers
+# All are all workers so we can drop this
+
+# # Check flags
+# oxford %>%
+#   select(ends_with("flag")) %>%
+#   map(~tabyl(.))
+
+oxford <- read_csv(url("https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_nat_latest.csv")) %>%
+  rename_to_lower_snake() %>%
+  filter(country_code == "UGA") %>%
+  select(date, matches("[ce]\\d")) %>%
+  filter(date < 20220725) %>% # No information available
+  mutate(year = year(ymd(date)), month = month(ymd(date)), day = day(ymd(date))) %>% # splitting date into year month and day
+  select(-date) %>% # no longer needed
+  select(-starts_with("c8"), -starts_with("e4")) %>% # do not care about international
+  select(-starts_with("e3")) %>% # minimal variation
+  select(-matches("c[1234]m_flag"), -e1_flag) %>% # No action for these flags, so drop
+  # Shorten names
+  rename(
+    gov_close_transport_flag = c5m_flag,
+    gov_stay_home_flag       = c6m_flag,
+    gov_restrict_move_flag   = c7m_flag
+  ) %>%
+  rename_with(
+    ~ str_replace(., "[ce]\\d.*?_", "gov_") # Change prefix for easier reading
+  ) %>%
+  rename(
+    gov_close_transport = gov_close_public_transport,
+    gov_restrict_gather = gov_restrictions_on_gatherings,
+    gov_restrict_move   = gov_restrictions_on_internal_movement,
+    gov_stay_home       = gov_stay_at_home_requirements
+  ) %>%
+  # Indicator for whether any restriction is targeted to a specific group/area.
+  # No information on which areas are targeted. 1: targeted 0: Not targeted/no
+  # restriction
+  mutate(
+    across(
+      ends_with("_flag"),
+      ~ case_when(
+        .x == 0 ~ 1,
+        .x == 1 ~ 0,
+        TRUE ~ 0
+      )
+    )
+  ) %>%
+  rename_with(
+    ~ str_replace(., "_flag", "_target")
+  ) %>%
+  group_by(year, month) %>%
+  summarise(
+    across(starts_with("gov_"),
+      list(
+        mode = ~ Mode(.x, na.rm = TRUE),
+        min  = ~ min(.x, na.rm = TRUE),
+        max  = ~ max(.x, na.rm = TRUE)
+      ),
+      .names = "{.col}_{.fn}"
+    )
+  )
+
+# Combine data frames ----
+
+covid_oxford <- full_join(covid_cases, oxford, by = c("year", "month"))
+
+covid_oxford %>% 
+  write_rds(here("data", "covid_oxford.rds"))
+
+covid_oxford %>% 
+  write_dta(
+    here("data", "covid_oxford.dta"),
+    version = 14,
+  )
+  
+
