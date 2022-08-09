c6dae34037750adc6bd2dbb94c65bbbbc9784589
Claus Portner <cportner@seattleu.edu>
2022-08-09 11:13:00 -0700

    Add poverty and land size information

--- 03_load_pre_covid_hh_data.R
+++ 03_load_pre_covid_hh_data.R
@@ -16,9 +16,33 @@ library(glue)
 
 # HHID correspondance ----
 hhid_correspondance  <- read_dta(here( "raw_data", "round1", "Cover.dta")) %>% 
-  select(HHID, baseline_hhid) %>% 
-  rename_to_lower_snake()
+  rename_to_lower_snake() %>% 
+  select(hhid, baseline_hhid)  
 
 # Income/poverty data ----
 
-poverty <- read_dta(here("raw_data", "panel_19_20", "pov2019_20.dta"))
+poverty <- read_dta(here("raw_data", "panel_19_20", "pov2019_20.dta")) %>% 
+  rename_to_lower_snake() %>% 
+  mutate(
+    food_poor_2020 = welfare <= hpline
+  ) %>% 
+  rename(
+    poor_2020_quints = quints
+  ) %>% 
+  select(
+    hhid, welfare, food_poor_2020, poor_2020, poor_2020_quints
+  )
+
+
+# Land size ----
+
+land <- read_dta(here("raw_data", "panel_19_20", "Agric", "agsec2a.dta")) %>% 
+  rename_to_lower_snake() %>% 
+  filter(!is.na(s2aq4) & s2aq11b != 3) %>%  # size available and not rented out
+  group_by(hhid) %>% 
+  summarise(
+    land_size = sum(s2aq4) # land in acres
+  )
+
+
+
