a2fb019f3ac9a5473a57477ae32d9f6374c4e20b
Claus Portner <cportner@seattleu.edu>
2023-09-22 11:37:31 -0700

    Compare with random effects

--- code/test_ols_vs_fx.R
+++ code/test_ols_vs_fx.R
@@ -28,7 +28,7 @@ ols <- map(
     tidy() %>%
     select(term, estimate, std.error, p.value) %>%
     mutate(variable = .x) %>%
-    select(variable, everything()) %>% mutate(estimate = round(estimate, 3), p.value = round(p.value, 3)) %>%
+    select(variable, everything()) %>%
     rename(ols_estimate = estimate, ols_std.error = std.error, ols_p.value = p.value)
 )
 
@@ -43,31 +43,68 @@ fx <- map(
     tidy() %>% 
     select(term, estimate, std.error, p.value) %>% 
     mutate(variable = .x) %>% 
-    select(variable, everything()) %>% mutate(estimate = round(estimate, 3), p.value = round(p.value, 3)) %>% 
+    select(variable, everything()) %>% 
     rename(fx_estimate = estimate, fx_std.error = std.error, fx_p.value = p.value)
 )
 
-# Turn ols and fx into tibbles
+# Random effects regress each of the food variables against survey dummies and cases
+re <- map(
+  food_vars, 
+  ~ plm(as.formula(paste0(.x, " ~ lockdown_1 + lockdown_2 + lockdown_7 + cases_smooth_per_100000")), 
+        data = base, 
+        index = c("hhid", "survey"), 
+        model = "random"
+  ) %>% 
+    tidy() %>% 
+    select(term, estimate, std.error, p.value) %>% 
+    mutate(variable = .x) %>% 
+    select(variable, everything()) %>% 
+    rename(re_estimate = estimate, re_std.error = std.error, re_p.value = p.value)
+)
+
+
+# Turn ols, fx, and re into tibbles
 ols <- ols %>% 
   map_dfr(as_tibble)
 fx <- fx %>% 
   map_dfr(as_tibble)
+re <- re %>%
+  map_dfr(as_tibble)
 
 
-# Join ols and fx
+# Join ols, fx, and re
 ols_vs_fx <- ols %>% 
   left_join(fx, by = c("variable", "term")) %>% 
+  left_join(re, by = c("variable", "term")) %>%
   # Calculate the difference between ols and fx using a z test with the standard error to test for difference
   # https://stats.stackexchange.com/questions/93540/testing-equality-of-coefficients-from-two-different-regressions
   # Clogg et al. 1995 and Paternoster et al. 1998
   mutate(
-    diff = ols_estimate - fx_estimate,
-    se = sqrt(ols_std.error^2 + fx_std.error^2),
-    z = diff / se,
-    p.value = 2 * pnorm(-abs(z)),
+    diff_ols_fx = ols_estimate - fx_estimate,
+    se_ols_fx = sqrt(ols_std.error^2 + fx_std.error^2),
+    z_ols_fx = diff_ols_fx / se_ols_fx,
+    p.value_ols_fx = 2 * pnorm(-abs(z_ols_fx)),
     # Calculate differences in ols_std.error and fx_std.error
-    diff_se = ols_std.error - fx_std.error
+    diff_se_ols_fx = ols_std.error - fx_std.error
+  ) %>% 
+  # Calculate the difference between fe and re using a z test with the standard error to test for difference
+  # https://stats.stackexchange.com/questions/93540/testing-equality-of-coefficients-from-two-different-regressions
+  # Clogg et al. 1995 and Paternoster et al. 1998
+  mutate(
+    diff_fx_re = fx_estimate - re_estimate,
+    se_fx_re = sqrt(fx_std.error^2 + re_std.error^2),
+    z_fx_re = diff_fx_re / se_fx_re,
+    p.value_fx_re = 2 * pnorm(-abs(z_fx_re)),
+    # Calculate differences in fx_std.error and re_std.error
+    diff_se_fx_re = fx_std.error - re_std.error
   ) 
+  
+# There are no statistically significant differences between the fixed effects
+# and OLS estimates, but the standard errors for the fixed effects are smaller
+# than for OLS.There is also no statistically significant difference between
+# fixed effects and randome effects, but the standard errors for fixed effects
+# are slightly higher, although this difference is miniscule.
 
+# Currently do not incorporate weights.
 
 
