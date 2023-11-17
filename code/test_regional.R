ea8cc0bbb0355aea51c099b68e3b76be821bbea8
Claus Portner <cportner@seattleu.edu>
2023-11-17 06:52:00 -0800

    Minor edits

--- code/test_regional.R
+++ code/test_regional.R
@@ -154,8 +154,6 @@ datasummary_correlation(national[, -1])
 
 cor(national[, -1], use = "pairwise.complete.obs")
 
-cor(regional[, -1], use = "pairwise.complete.obs")
-
 regional %>% 
   select(-date) %>% 
   group_by(region) %>% 
